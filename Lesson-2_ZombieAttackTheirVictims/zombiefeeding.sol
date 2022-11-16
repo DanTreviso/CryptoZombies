6619e8823483841dff6154934c86bc88d280f887
DanTreviso <noal.daniel@gmail.com>
2022-11-16 18:34:33 +0000

    First version of the files
    Introduction to Solidity following tutorial

---
+++ Lesson-2_ZombieAttackTheirVictims/zombiefeeding.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.25;
+import "./zombiefactory.sol";
+contract KittyInterface {                 // to be able to call function getKitty from the other contract
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]); // check that I own this zombie
+    Zombie storage myZombie = zombies[_zombieId];    // get the dna of this zombie
+    _targetDna = _targetDna % dnaModulus; // check _targetDna is not bigger than 16 digits
+    uint newDna = (myZombie.dna + _targetDna) / 2;  // newDna calculates as average between the 2 dnas
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
+}
